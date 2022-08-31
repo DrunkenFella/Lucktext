8c071cd59c5ad45bf9afb0da369824f8faae8823
Drunken Fella <alex.ostlund@elev.ga.ntig.se>
2022-08-31 14:26:06 +0200

    ,mjyjy

---
+++ Lucktext/Lucktext.cs
@@ -0,0 +1,21 @@
+﻿
+string name;
+string age;
+string person;
+string place;
+
+Console.WriteLine("What's your name?");
+name = Console.ReadLine ();
+
+Console.WriteLine("What's your age");
+age = Console.ReadLine ();
+
+Console.WriteLine("What's your least liked person?");
+person = Console.ReadLine ();
+
+Console.WriteLine("What is your favorit place to be in?");
+place = Console.ReadLine ();
+
+Console.WriteLine($"Today {name} died in {place} at the age of {age} by the hands of {person}. They will not be missed.");
+Console.ReadLine ();
+
